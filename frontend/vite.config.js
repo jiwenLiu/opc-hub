cee22c6eb7b7dd36b8aa3b225dd8f283b27cddb4
opc-hub-user <734655945@qq.com>
2026-04-21 16:14:46 +0800

    前端代理改127.0.0.1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://172.20.144.155:8080',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true
       }
     }
