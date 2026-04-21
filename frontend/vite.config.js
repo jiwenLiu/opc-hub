6b4dd351901d34a0deddb2c0561323268ba36fbf
opc-hub-user <734655945@qq.com>
2026-04-21 13:33:48 +0800

    更新阿里云ECS数据库配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://172.20.144.155:8080',
         changeOrigin: true
       }
     }
